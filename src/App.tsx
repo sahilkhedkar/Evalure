110d003a98155b0b0ba3846edc8c6a436ed4355b
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-07 23:36:30 +0530

    updated the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
+    <main className='bg-[url("/images/bg-main.svg")] bg-cover'>
+      <Navbar/>
     <section className='main-section'>
       <div className='page-heading'>
         <h1>Track your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
     </section>
+    </main>
   )
 }
 
