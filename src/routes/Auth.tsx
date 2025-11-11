3f8d97975d09d9e71f7ce9381bb2443fdd5dd8f5
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-11 22:48:11 +0530

    created the nice login component

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,10 +1,50 @@
-
+import { useEffect } from "react";
+import { usePuterStore } from "../lib/puter";
 
 const Auth = () => {
+
+  const {isLoading , auth} = usePuterStore()
+
+   useEffect(() => {
+    document.title = "Evalure | Auth";
+  }, []);
+
+  useEffect(() => {
+
+  },[auth.isAuthenticated])
+
   return (
-    <div>
-      Auth
-    </div>
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-2 text-center">
+              <h1>Welcome</h1>
+              <h2>Log in to continue your Job Journey</h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                  <p> Signing you in ... </p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p> Log out </p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Log in</p>
+                  </button>
+                )
+              }
+              </>
+            )
+          }
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
 
