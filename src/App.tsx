1873c41815c53e58d401e2899896f5512c24f98d
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-11 22:15:35 +0530

    structured a little better

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./routes/Home"
-import Auth from "./routes/Auth"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Auth from "./routes/Auth";
 
 function App() {
-
   return (
- <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
