9522f9d741bc71b950cd175159a16a78a01524e4
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-11 23:05:25 +0530

    added the direct login in the home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,29 +1,41 @@
-
-import '../App.css'
-import Navbar from '../components/Navbar'
-import ResumeCard from '../components/ResumeCard'
-import { resumes } from '../constants'
+import { useEffect } from "react";
+import { usePuterStore } from "../lib/puter";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import Navbar from "../components/Navbar";
+import ResumeCard from "../components/ResumeCard";
+import { resumes } from "../constants";
 
 function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = "Evalure | Auth";
+  }, []);
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+
   return (
-      <main className='bg-[url("/images/bg-main.svg")] bg-cover'>
-      <Navbar/>
-    <section className='main-section'>
-      <div className='page-heading py-16'>
-        <h1>Track your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedback</h2>
-      </div>
-      {resumes.length > 0 && (
-      <div className='resumes-section'>
-        {resumes.map((resume) => (
-          <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-    )}
-    </section>
-    
+    <main className='bg-[url("/images/bg-main.svg")] bg-cover'>
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
