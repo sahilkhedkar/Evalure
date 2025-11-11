1247c96887bac2667947380ebb3f6df9f4428df1
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-11 23:03:32 +0530

    fixed a bug in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Auth from "./routes/Auth";
+import { usePuterStore } from "./lib/puter";
+import { useEffect } from "react";
 
 function App() {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init();
+  }, [init]);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
